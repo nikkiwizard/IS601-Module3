e020342f1a88a4088c758998562b7e220b6a7dad
nikkiwizard <nc442@njit.edu>
2026-06-08 20:17:43 -0400

    updated operations imports

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,12 +1,12 @@
 import pytest
-from app.operations import addition, subtraction, multiplication, division
+from app.operations import Operations
 
-#parametrized yay
+# parametrized using Operations static methods
 @pytest.mark.parametrize("func,a,b,expected", [
-    (addition, 1, 1, 2),
-    (subtraction, 1, 1, 0),
-    (multiplication, 2, 3, 6),
-    (division, 6, 2, 3),
+    (Operations.addition, 1, 1, 2),
+    (Operations.subtraction, 1, 1, 0),
+    (Operations.multiplication, 2, 3, 6),
+    (Operations.division, 6, 2, 3),
 ])
 def test_operations(func, a, b, expected):
     assert func(a, b) == expected
@@ -15,4 +15,4 @@ def test_operations(func, a, b, expected):
 def test_division_by_zero():
     """Test division by zero."""
     with pytest.raises(ValueError, match="Division by zero is not allowed."):
-        division(1, 0)
+        Operations.division(1, 0)

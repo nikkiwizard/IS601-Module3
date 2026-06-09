75e2428e8f8577a69fd71c0167aefb3b37503c7e
nikkiwizard <nc442@njit.edu>
2026-06-08 20:11:40 -0400

    made operations class

--- app/operations/__init__.py
+++ app/operations/__init__.py
@@ -1,13 +1,21 @@
-def addition(a:float, b:float) -> float:
-    return a + b
+class Operations:
+    """Making a class to hold all the operations, yay for OOP
+    """
 
-def subtraction(a:float, b:float) -> float:
-    return a - b
+    @staticmethod
+    def addition(a: float, b: float) -> float:
+        return a + b
 
-def multiplication(a:float, b:float) -> float:
-    return a * b
+    @staticmethod
+    def subtraction(a: float, b: float) -> float:
+        return a - b
 
-def division(a:float, b:float) -> float:
-    if b == 0:
-        raise ValueError("Division by zero is not allowed.")
-    return a / b
+    @staticmethod
+    def multiplication(a: float, b: float) -> float:
+        return a * b
+
+    @staticmethod
+    def division(a: float, b: float) -> float:
+        if b == 0:
+            raise ValueError("Division by zero is not allowed.")
+        return a / b
